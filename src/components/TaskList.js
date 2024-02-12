b539fbeaf628e99724683bac2bcfc6b96891886d
Kenildev007 <kenilgorasya6@gmail.com>
2024-02-12 15:45:56 +0530

    add,save with options,delete completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -11,10 +11,9 @@ const TaskList = ({tasks , deleteTask}) => {
         <div>
             {tasks.map((task, index) => (
                 <div key={index} className='flex justify-between mx-20 my-3 p-2 border outline-1-black'>
-                    <div className='left-infos'>{task}</div>
-
+                    <div className='left-infos'>{task.text}</div>
                     <div className='flex items-center gap-4 mx-2'>
-                        <input type="checkbox" id="TaskCompleted" />
+                        <span>{task.status}</span>
                         <FaPencil size={18} />
                         <AiFillDelete size={22} onClick={() => handleDelete(index)} />
                     </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 
 const TodoInput = ({ addTask }) => {
     const [taskText, setTaskText] = useState('');
+    const [taskStatus, setTaskStatus] = useState('completed'); // Initialize with a default value
 
     const handleInputChange = (e) => {
         setTaskText(e.target.value);
     }
 
+    const handleSelectChange = (e) => {
+        setTaskStatus(e.target.value);
+    }
+
     const handleAddTask = () => {
         if(taskText.trim()) {
-            addTask(taskText);
+            addTask({ text: taskText, status: taskStatus }); // Pass both text and status
             setTaskText('');
         }
     };  
+
     return (
         <div className='flex justify-center text-center gap-5 p-5 m-5'>
             <input
@@ -21,11 +27,16 @@ const TodoInput = ({ addTask }) => {
                 placeholder="Enter Task" 
                 value={taskText}
                 onChange={handleInputChange}
-                /> 
-                <br />
+            /> 
+            <select className='border' name="taskStatus" id="taskStatus" onChange={handleSelectChange} value={taskStatus}>
+                <option value="completed">Completed</option>
+                <option value="pending">Pending</option>
+                <option value="error">Error</option>
+                <option value="probably">Probably</option>
+            </select>
             <button className='TaskAddBtn' onClick={handleAddTask}>Add Task</button>
         </div>
     );
-};
+};  
 
 export default TodoInput;
